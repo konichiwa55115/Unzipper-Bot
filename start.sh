e2f229708ed8727ef76efa6f75085934b4fb45d7
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-15 10:33:03 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,4 +9,9 @@ Copyright (c) 2022 Itz-fork
 --> Join @NexaBotsUpdates
 --> Follow Itz-fork on Github
 "
+echo "Cloning Repo...."
+git clone https://github.com/konichiwa55115/Unzipper-Bot /LazyDeveloper
+cd /LazyDeveloper
+pip3 install -r requirements.txt
+echo "Starting Bot...."
 python3 -m unzipper
