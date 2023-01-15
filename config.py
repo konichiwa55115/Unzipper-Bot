e27ae07bacf7468ffa85f5cbe3274f18aa999779
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-15 10:06:05 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ import os
 
 class Config(object):
     # Mandotory
-    APP_ID = int(os.environ.get("APP_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL"))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-    MONGODB_URL = os.environ.get("MONGODB_URL")
+    APP_ID = "17983098"
+    API_HASH = "ee28199396e0925f1f44d945ac174f64"
+    BOT_TOKEN = "5596598781:AAEVMvT3LNz2wrSYiy69CbuOPvHAcTkmKvg"
+    LOGS_CHANNEL = "-1001683878954"
+    BOT_OWNER = "1227193881"
+    MONGODB_URL = "mongodb+srv://Bala7a19871:Ibntaymya1.@cluster0.5mtsc.mongodb.net/?retryWrites=true&w=majority"
     GOFILE_TOKEN = os.environ.get("GOFILE_TOKEN")
     # Optional
     MAX_DOWNLOAD_SIZE = int(os.environ.get("MAX_DOWNLOAD_SIZE")) if os.environ.get("MAX_DOWNLOAD_SIZE") else 10737418240
