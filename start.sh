f01728352d58ec2e5cd558dfd69ad99f47917a4e
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-15 11:13:58 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,3 @@
-#!/usr/bin/bash
-
-VERSION="v1.0"
-echo "
-    ||| Unzipper Bot - $VERSION |||
-
-Copyright (c) 2022 Itz-fork
-
---> Join @NexaBotsUpdates
---> Follow Itz-fork on Github
-"
 echo "Cloning Repo...."
 git clone https://github.com/konichiwa55115/Unzipper-Bot /LazyDeveloper
 cd /LazyDeveloper
