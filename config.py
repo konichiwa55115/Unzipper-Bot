718077355cd6cbef77c9d430edfbdd9a97ef76cc
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-02-01 19:44:22 +0200

    Update config.py

--- config.py
+++ config.py
@@ -14,6 +14,6 @@ class Config(object):
     # Optional
     MAX_DOWNLOAD_SIZE = int(os.environ.get("MAX_DOWNLOAD_SIZE")) if os.environ.get("MAX_DOWNLOAD_SIZE") else 10737418240
     # Constents
-    DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/NexaBots"
+    DOWNLOAD_LOCATION = f"./NexaBots"
     TG_MAX_SIZE = 2040108421
     CHUNK_SIZE = 1024 * 6
