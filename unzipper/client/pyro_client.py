dd5f24b7a34efb16c6a3410ed84a9fd66b598396
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-02-01 21:16:26 +0200

    Update pyro_client.py

--- unzipper/client/pyro_client.py
+++ unzipper/client/pyro_client.py
@@ -39,7 +39,7 @@ class UnzipperBot(Client):
                          api_id=Config.APP_ID,
                          api_hash=Config.API_HASH,
                          bot_token=Config.BOT_TOKEN,
-                         plugins=dict(root="unzipper/modules"),
+                         plugins=plugins,
                          sleep_threshold=10)
 
     ######## Decorators ########
