8db33f3826130c974395ecb86fe7d287198d8438
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-02-01 21:22:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     # Mandotory
     APP_ID = "17983098"
     API_HASH = "ee28199396e0925f1f44d945ac174f64"
-    BOT_TOKEN = "6117473467:AAHgj3sCqGc23LwIsetiEkq5L9Fug84qf9c"
+    BOT_TOKEN = "6089522865:AAG0vUsYSBg-BaSfiG1T5E1QTrorjoMDA4U"
     LOGS_CHANNEL = int(-1001683878954)
     BOT_OWNER = "1227193881"
     MONGODB_URL = "mongodb+srv://Bala7a19871:Ibntaymya1.@cluster0.5mtsc.mongodb.net/?retryWrites=true&w=majority"
