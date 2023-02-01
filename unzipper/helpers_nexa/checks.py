25a9b105521a856806a082c66761e3095d67d8b6
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-02-01 19:52:16 +0200

    Update checks.py

--- unzipper/helpers_nexa/checks.py
+++ unzipper/helpers_nexa/checks.py
@@ -12,7 +12,7 @@
 
 
 import logging
-from pyrogram import enums
+from pyrogram.enums import chat_type
 from unzipper import unzip_client
 from config import Config
 
@@ -21,7 +21,7 @@ def check_log_channel():
     try:
         if Config.LOGS_CHANNEL:
             c_info = unzip_client.get_chat(chat_id=Config.LOGS_CHANNEL)
-            if c_info.type != enums.ChatType.CHANNEL:
+            if c_info.type != chat_type.ChatType.CHANNEL:
                 logging.warn("Chat is not a channel!")
                 exit()
             elif c_info.username is not None:
